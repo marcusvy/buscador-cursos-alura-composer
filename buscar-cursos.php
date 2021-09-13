08def9cdf18886cc7f5e0487fcf2d24654cfb4c3
Marcus Vinicius <marcusvy@gmail.com>
2021-09-13 19:00:41 -0400

    first commit

---
+++ buscar-cursos.php
@@ -0,0 +1,18 @@
+#!/usr/bin/env php
+<?php
+
+use App\Buscador;
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+$client = new Client(['verify' => false]);
+
+$crawler = new Crawler();
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('https://www.alura.com.br/cursos-online-programacao/php');
+array_map(function ($curso) {
+  echo $curso . PHP_EOL;
+}, $cursos);
---
+++ src/Beleza.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+class Beleza
+{
+  public static function mostrar()
+  {
+    for ($x = 0; $x < 1000; $x++) {
+      echo "Beleza: " . $x . PHP_EOL;
+    }
+  }
+}
